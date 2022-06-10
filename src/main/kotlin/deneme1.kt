4f341f2eaaec9df2e94fd50b165abd9857a82640
corepavlion <corepavlion@gmail.com>
2022-06-10 22:49:35 +0300

    first commit

--- src/main/kotlin/deneme1.kt
+++ src/main/kotlin/deneme1.kt
@@ -2,5 +2,13 @@ fun main() {
 //    val (a, b, c, d, e) = readln().split(" ")
 //    print("$a $b $c $d $e")
 
-    print("${readln()} ${readln()} ${readln()} ${readln()} ${readln()}")
+   // print("${readln()} ${readln()} ${readln()} ${readln()} ${readln()}")
+
+    println(factorial(7))
+}
+
+//faktoriyel algoritmasi recursive func
+fun factorial(num: Int): Int {
+    if (num <= 1) return 1
+    return num * factorial(num - 1)
 }
