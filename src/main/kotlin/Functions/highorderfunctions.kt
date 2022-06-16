1ed4ac73d9c638ca9843ac9ffc683835d4bac13a
corepavlion <corepavlion@gmail.com>
2022-06-16 21:12:05 +0300

    first comit

--- src/main/kotlin/Functions/highorderfunctions.kt
+++ src/main/kotlin/Functions/highorderfunctions.kt
@@ -1,9 +1,31 @@
 package Functions
+import java.util.*
+import kotlin.concurrent.schedule
+
+//fun main(args: Array<String>) {
+//    printName("Gökhan")
+//
+//    printTotalName("${printName("Gökhan")}")
+//    printTotalName("String")
+//    printTotalName({
+//        return  "Ad : $name"
+//    })
+//}
+//
+//
+//fun printName(name: String): String{
+//    return  "Ad : $name"
+//}
+//
+//fun printTotalName(totalName: String){
+//    println(totalName)
+//}
 
 fun main() {
+
     val name = "mustafa"
     val surnmae = "unlu"
-    val fullName = name + " " + surnmae
+    val fullName = "$name $surnmae"
 
     fun deneme(fullName: String) : String{
         return "Merhaba $fullName"
@@ -12,20 +34,262 @@ fun main() {
         return "Merhaba $fullName"
     }
 
-    val welcomeMessage = deneme(fullName)
-
-    deneme(deneme2(fullName))
+    val welcomeMessage = deneme(fullName) //fonksiyonlar bir degiskene atanabiliyor. yada fonksiyona geru gibys degeri olarak verilebilir.
+    deneme(deneme2(fullName)) // fonksiyonlar baska bir fonksiyona parametre olarak verilebilir.
 
     //buraya kadar first class citizen function mevzusu normal kotlindeki functionarin ozellikleridir.
 
-    //higher order functionlar ise en basit olarak fonksiyon bodysidir diyebiliriz.
-    //higher orderin interface  gore artisi nedir.
-    //higher order fonksiyonlarda parametre kismina veya geri donus kismina fonksiyon bodysini verebiliyoruz.
-                //funcname     :  //varName: vartype(burasi aslinda functype)     //returntype
-    fun foo(higherOrderFunction: (message:String)->Unit){
-        higherOrderFunction("Codemy")
+
+
+
+
+    //higher orderin interface  gore artisi nedir.??
+
+    /**
+     *      Fonksiyon'lar Kotlin'de "First Class Citizen"dır. Yani degiskenlere deger olarak atanabilir, baska fonksiyonlara
+     *      parametre olarak verilebilir ya da bir fonksiyonunn geri donus degeri olabilir demektir.
+     *
+     *      Higher Order Function'lar basitce bir fonksiyona parametre olarak verilen fonksiyonlardır. Parametre olarak
+     *      verilmekten kasit, fonksiyonun cagriminin parametre kisminda yapilmasi degil, fonksiyonun govdesinin (body)
+     *      yani suslu parantezler arasinda kalan gorev alaninin baska bir fonksiyona parametre olarak verilmesidir.
+     *
+     *      Yapisal olarak;
+     *
+     *      fun foo(higherOrderFunction: (message: String) -> Unit){
+     *          some business logics
+     *          some business logics
+     *          some business logics
+     *          some business logics
+     *          higherOrderFunction("Codemy")
+     *      }
+     *
+     *
+     *      Cagrilirken ;
+     *
+     *      fun main(){
+     *
+     *          foo({ message ->
+     *              println("Message : $message")
+     *          })
+     *      }
+     */
+
+
+
+/* -------------------------------------------------------------------------------------------------------------------*/
+
+    /**
+     *      Higher Order Function'lari tanimlamanin 3 yolu vardir.
+     */
+
+    // Bir degiskene atayarak Higher Order Function tanimlayabilirsiniz.
+    // Bu durumda suslu parantezler yanina higher order function'in aldigi parametreler lambda okundan once aralarina virgul
+    // koyularak yazilir. Higher Order Function tek parametre aliyorsa, bu parametreleri yazmak zorunda degilsinizdir.
+    // Bu durumda higher order function size "it" kelimesi ile higher order function'in parametresi tipinde bir degisken verecektir.
+    val higherOrderFunction = { surName: String ->
+        "surName : $surName"
     }
 
-    //cagirirken;
-    foo({message -> println("Message: $message")  })
+
+    // Ismi olmayan "anonymous function" tanimlamalari da Higher Order Function olarak, normal bir fonksiyona parametre
+    // olarak verilebilir.
+    val anonymousFunction = fun(surName: String): String {
+        return "surName : $surName"
+    }
+
+    // Anonymous Function'in expression kullanimini da yine Higher Order Function olarak normal bir fonksiyona parametre
+    // olarak verilebilir.
+    val anonymousFunction2 = fun(surName: String): String = "surName : $surName"
+
+
+
+
+    // Higher Order Function'la ayni parametre sayisina sahip ve bu parametrelerin hepsinin tipleri Higher Order Function'in
+    // parametre tipleri ile ayni ise, bu normal fonksiyon da Higher Order Function olarak normal bir fonksiyona parametre
+    // olarak verilebilir. Bunu yapmak icin sadece basina :: isareti koymak yeterlidir.
+
+    fun logPrint(message: String, count: Int): String{
+        return "Log: $message"
+    }
+
+    fun foo(higherOrderFunction: (message: String, count: Int) -> String){
+        higherOrderFunction("Codemy")
+    }
+    logPrint("hersey yolunda", 24)
+    foo(::logPrint)
+
+
+
+    val news = News()
+    news.read(::println)
+    news.read({title ->
+        print("asdsgf")
+    })
+    news.read { title ->
+        print(title)
+    }
+
+    news.read {
+        print(it)   //it = title
+    }
+
+
+    val titleFun = fun(title:String): Unit {
+        print(title)
+    }
+
+    news.read(titleFun) //higher orderi parametre olarak alan extension function infixte yapabiliriz
+
+
+    printUserInfo(getName(), higherOrderFunction, getAge())
+    printUserInfo(getName(), anonymousFunction, getAge())
+    printUserInfo(getName(), fun(surName: String): String {
+        return "surName : $surName"
+    }, getAge())
+
+    // Suslu parantezler (body) direkt olarak Higher Order Function'in parametre olarak beklendigi alana da yazilabilir.
+    printUserInfo(getName(), {
+        "surname : $it"
+    }, getAge())
+
+    getItemClickListener({ buttonName ->
+        println("$buttonName tıklandı..")
+    })
+
+/* -------------------------------------------------------------------------------------------------------------------*/
+
+    /**
+     *      Eger bir fonksiyon parametreleri icerisinde bir Higher Order Function son parametre olarak tanimlandiysa, bu durumda
+     *      -isteniyorsa, bu Higher Order Function, fonksiyon parametrelerinin disina da yazilabilir. Daha temiz bir kullanim
+     *      yapmis oluruz.
+     */
+    val newsType = NewsType()
+    news.getNewsFromServer("FoxTv", newsType, {
+        println(this.toString())
+    }) // Higher Order Function, fonksiyon parametrelerinin icerisinde tanimlanmistir.
+
+    news.getNewsFromServer("FoxTv", newsType) {
+        println(this.toString())
+    } // Higher Order Function, fonksiyon parametrelerinin disinda tanimlanmistir.
+
+/* -------------------------------------------------------------------------------------------------------------------*/
+
+    /**
+     *      Higher Order Function birde fazla parametre iceriyorsa, aralarina virgul koyarak lambda isareti ile tanimlamak zorunlu.
+     *      Tek parametreye sahipse bu durumda parametre ismi ve lambda isareti koyulmayabilir. Bu durumda ilgili parametre "it"
+     *      kelimesi ile cagrilabilecektir.
+     *      Eger bir fonksiyon, parametre olarak sadece Higher Order Function aliyorsa bu durumda fonksiyon parantezlerini hic yazmayabilirsiniz.
+     */
+    news.filterNews { filterType, getFilterName ->
+        print("asdsgf")
+    }
 }
+
+/* -------------------------------------------------------------------------------------------------------------------*/
+
+// Normal Fonksiyon
+fun getName(): String {
+    return "Gökhan"
+}
+
+// Normal Fonksiyon, expression kullanima ornek
+fun getAge(): Int = 29
+
+/* -------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ *      2. parametre Higher Order Fonksiyon olarak tanimlanmistir.
+ *      Higher Order Fonksiyonlar default deger alabilirler. Bunun icin basitce suslu parantezler acmak yeterlidir.
+ *      Dikkat edilmesi gereken konu bu suslu parantezler icine, Higher Order Function parametre bekliyorsa, bunlar verilmelidir.
+ */
+fun printUserInfo(name: String, getSurName: (surName: String) -> String = { surName -> "" }, age: Int): Unit {
+    println("name: $name , age : $age")
+
+    println(getSurName("ÖZTÜRK"))
+}
+
+/* -------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ *      Higher Order Function'a parametre tanimlarken sadece degisken tipini tanimlayarak da (degisken adi olmadan) kullanilabilir.
+ */
+fun getItemClickListener(onClick: (String) -> Unit) {
+
+    println("Tiklama islemi baslatiliyor")
+
+    Timer().schedule(3000) {
+
+        // Bir higher order function, parametre olarak yazildiktan sonra, bu parametrenin normal fonksiyon icerisinde
+        // cagirilmasi gerekmektedir. Aksi halde bu higher order fonksiyonu tanimlamak mantiksiz olur. Normal fonksiyonunun
+        // cagrildigi alandaki higher order function'in body kismi hicbir zaman cagrilmaz demek olur.
+        onClick("Login")
+
+        println("Tiklama islemi bitti")
+    }
+}
+
+/* -------------------------------------------------------------------------------------------------------------------*/
+
+class News {
+    fun getNewsType(newsType: NewsType): String {
+        return when (newsType.toString()) {
+            NewsType.breakingNews -> "Breaking"
+            NewsType.urgent -> "Urgent"
+            NewsType.local -> "Local"
+            NewsType.global -> "Global"
+            else -> "Normal"
+        }
+    }
+}
+
+class NewsType {
+    companion object {
+        val breakingNews = "BreakingNews"
+        val urgent = "Urgent"
+        val local = "Local"
+        val global = "Global"
+        val normal = "Normal"
+    }
+}
+
+/**
+ *      Bir Higher Order Function'ina parametre verirken Classismi.() seklindde tanimlama yapilabilir.
+ *      Bu sayede ilgili class da parametre parantezi icerisine yazilabilir.
+ */
+fun News.getNewsFromServer(channelType: String, newsType: NewsType, getNews: NewsType.(Int) -> Unit) {
+    when (channelType) {
+        "KanalD" -> {
+            getNews(newsType, 1)
+        }
+        "ShowTv" -> {
+            getNews(newsType, 2)
+        }
+        "Tv8" -> {
+            getNews(newsType, 3)
+        }
+        "FoxTv" -> {
+            getNews(newsType, 3)
+        }
+        "CNN" -> {
+            getNews(newsType, 3)
+        }
+    }
+}
+
+/* -------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ *      Bir Higher Order Function'sın parametresi de yine Higher Order Function olabilir.
+ */
+infix fun News.filterNews(getFilter: (filterType: String, getFilterName: () -> String) -> Unit) {
+
+    getFilter("filterName") {
+        "String return label"
+    }
+}
+
+fun News.read(readTitle: (title:String) -> Unit) {
+    readTitle("Codemy is Awesome")
+}
+
+
---
+++ src/main/kotlin/denee.kt
@@ -0,0 +1,155 @@
+
+//    val c1 = readln().first()
+//    val c2 = readln().first()
+//    val c3 = readln().first()
+//    val c4 = readln().first()
+//
+//    println(c1.isDigit())
+//    println(c2.isDigit())
+//    println(c3.isDigit())
+//    println(c4.isDigit())
+//
+//
+// veya daha kisa yoldan yapalim
+//    repeat(4){
+//        println(readln().first().isDigit())
+//    }
+
+//    val mutList = mutableListOf<Char>('!', '@', '#', '$')
+//    println(mutList.last())
+//    println(mutList[mutList.size - 1])
+//    println(mutList[mutList.lastIndex])
+
+    const val camel = """Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!"""
+
+    const val lion = """Switching on the camera in the lion habitat...
+                                               ,w.
+                                             ,YWMMw  ,M  ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\
+,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
+WMMm__,-'.'     /      _.\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+"^MP__.-'    ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
+           /   .'            ; ;         )  )`{  \ `"^W^`,   \  :
+          /  .'             /  (       .'  /     Ww._     `.  `"
+         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+        (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
+The lion is roaring!"""
+
+    const val deer = """Switching on the camera in the deer habitat...
+   /|       |\
+`__\\       //__'
+   ||      ||
+ \__`\     |'__/
+   `_\\   //_'
+   _.,:---;,._
+   \_:     :_/
+     |@. .@|
+     |     |
+     ,\.-./ \
+     ;;`-'   `---__________-----.-.
+     ;;;                         \_\
+     ';;;                         |
+      ;    |                      ;
+       \   \     \        |      /
+        \_, \    /        \     |\
+          |';|  |,,,,,,,,/ \    \ \_
+          |  |  |           \   /   |
+          \  \  |           |  / \  |
+           | || |           | |   | |
+           | || |           | |   | |
+           | || |           | |   | |
+           |_||_|           |_|   |_|
+          /_//_/           /_/   /_/
+Our 'Bambi' looks hungry. Let's go to feed it!"""
+
+    const val goose = """Switching on the camera in the goose habitat...
+
+                                    _
+                                ,-"" "".
+                              ,'  ____  `.
+                            ,'  ,'    `.  `._
+   (`.         _..--.._   ,'  ,'        \    \
+  (`-.\    .-""        ""'   /          (  d _b
+ (`._  `-"" ,._             (            `-(   \
+ <_  `     (  <`<            \              `-._\
+  <`-       (__< <           :
+   (__        (_<_<          ;
+    `------------------------------------------
+The goose is staring intently at you... Maybe it's time to change the channel?"""
+
+    const val bat = """Switching on the camera in the bat habitat...
+_________________               _________________
+ ~-.              \  |\___/|  /              .-~
+     ~-.           \ / o o \ /           .-~
+        >           \\  W  //           <
+       /             /~---~\             \
+      /_            |       |            _\
+         ~-.        |       |        .-~
+            ;        \     /        i
+           /___      /\   /\      ___\
+                ~-. /  \_/  \ .-~
+                   V         V
+This bat looks like it's doing fine."""
+
+    const val rabbit = """Switching on the camera in the rabbit habitat...
+         ,
+        /|      __
+       / |   ,-~ /
+      Y :|  //  /
+      | jj /( .^
+      >-"~"-v"
+     /       Y
+    jo  o    |
+   ( ~T~     j
+    >._-' _./
+   /   "~"  |
+  Y     _,  |
+ /| ;-"~ _  l
+/ l/ ,-"~    \
+\//\/      .- \
+ Y        /    Y
+ l       I     !
+ ]\      _\    /"\
+(" ~----( ~   Y.  )
+It looks like we will soon have more rabbits!"""
+
+fun main() {
+
+    print("Please enter the number of the habitat you would like to view:>")
+    var read = readln().toInt()
+    val animal = arrayListOf(camel, lion, deer, goose, bat, rabbit )
+    println(animal[read])
+    println("---\n" +
+            "You've reached the end of the program. To check another habitat, please restart the watcher.")
+
+}
+
+
+
+
+
+
+
+
