9c4e26b14044c166d621277eec9e8685762f3ace
corepavlion <corepavlion@gmail.com>
2022-06-18 00:00:35 +0300

    first comit

---
+++ src/main/kotlin/denememee.kt
@@ -0,0 +1,58 @@
+fun main() {
+
+//        var count = 0
+//        do {
+//            val n = readln().toInt()
+//            if (n > count) count = n
+//        } while (n > 0)
+//        println(count)
+ 
+
+//    val numbers = mutableListOf<Int>()
+//    var number = readLine()!!.toInt()
+//    while (number != 0) {
+//        numbers.add(number)
+//        number = readLine()!!.toInt()
+//    }
+//    println(numbers.maxOrNull())
+
+
+//    var max = 0
+//    val num = readln().toInt()
+//    repeat(num){
+//        val input = readln().toInt()
+//        if(input % 4 == 0 && input >= max) max = input
+//    }
+//    println(max)
+
+
+//        var rec = 0
+//        val times = readln().toInt()
+//        repeat(times){
+//            val sayi = readln().toInt()
+//            if (sayi > 0)  rec++
+//        }
+//    println(rec)
+
+
+
+
+
+
+
+//    val n = readln().toInt()
+//    var sum = 0
+//
+//    repeat(n) {
+//        val next = readln().toInt()
+//        sum += next
+//    }
+//
+//    println(sum)
+}
+
+//fun Array<Any>.add(arr: Array<Any?>, element: Any): Array<Any?> {
+//    val array = arr.copyOf(arr.size + 1)
+//    array[arr.size] = element
+//    return array
+//}
