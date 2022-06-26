e32cd1b7c2ff5449757dbf560b44868b62611676
corepavlion <corepavlion@gmail.com>
2022-06-26 21:28:21 +0300

    first comit

--- src/main/kotlin/4 Classes/5 Inheritance.kt
+++ src/main/kotlin/4 Classes/5 Inheritance.kt
@@ -6,12 +6,15 @@ import java.awt.Color
  * her child class varsa o child class ana classdan daha fazla ozellige sahiptir.(her zaman degil)
  * ust classlar child classlardan her zaman memory olarak ddaha performanslidir.
  * hic kullanilmayan bir fonksiyonu ust classa vermek yerine miras alan child class a veririsin ve performansini etkilemezsin hemde okunakligi artar
- * bazen ust classin yetkinliklerini child classlar ile ihtiyaca uygun olarak degistirmek isteyebiliriz. yani ust classi sablon olarak kullanabiliriz. alt classda ozelligi degistiririz bunada override deriz.
- * ust classdaki bir beceri setini her zaman eklemeyebiliriz.
+ * bazen ust classin yetkinliklerini child classlar ile ihtiyaca uygun olarak degistirmek isteyebiliriz.
+ * yani ust classi sablon olarak kullanabiliriz.
+ * alt classda ozelligi degistiririz bunada override deriz.
+ * ust classdaki bir beceri setini her zaman eklemeyebiliriz.(superi her zaman eklemek istemeyiz)
  *
+ * inherit alirken ana class in istegidimiz constructori ile inherit alabiliriz.
  *
  * javada extends keywordu kullanilirken kotlinde inherit edilmesi icin class open olmalidir ve : operatoru ile inherit alinir.
- * kotlinde classlar default olarak finaldir. final olan class inherit edilemez. inherit edilmeyecekse final yazmaliyiz.
+ * kotlinde classlar default olarak finaldir. final olan class inherit edilemez. final yazmamaoza gerek yoktur default olarak finaldir.
  *
  * bazen fonksiyonlarda ve degiskenlerdede open keywordu olacak. Bunun sebebi class open olursa miras alinir
  * degisken veya fonksiyon open olursa bunlarin kendisini override edebliriz.
@@ -22,7 +25,9 @@ import java.awt.Color
  *
  * bazen ust class i miras alindigi gibi alt classida baska bir class miras alabilir.
  * bir child class ustundeki butun atalarinin private olmayan open olan butun ozelliklerini miras alir.
- * final override = final olur. buda override edilemez. ama cagirabiliriz sifirdan kendimiz olusturabilirz..
+ *
+ *access modifier; open ve finaldir (miras alinabilir alinamazligi ve override edilip edilemezi duzenler)
+ * visibility modifier; public private internal protected (encapsulation saglar)
  *
   */
 
@@ -59,18 +64,30 @@ class Rectangle(name: String) : Shape(name){   //miras alinan class in herhangi 
 
     override fun drawShape(){
 
-        super.drawShape() //ust classtaki fonksiyonuda calistirir. isimize yaramiyorsa gerek yok yazmayabiliriz.
-
+        super.drawShape() //ust classtaki(shape) fonksiyonuda calistirir. isimize yaramiyorsa gerek yok yazmayabiliriz.
+        //this.drawShape() // bagli olunan classtaki(rectangle) fonksiyonu calistiirr.
         calculateArea()
 
-        val square = "kare cizildi."
-        println(square)
+        val rectangle = "dikdortgen cizildi."
+        println(rectangle)
     }
+
+    override var mEdgeCount: Int
+        get() = 213213 // istersek super.mEdgeCount   dondurebiliriz.
+        set(value) {}
 
 
 
 }
 
-fun main() {
+open class Square(name: String,color: String,edgeCount: Int) : Shape(name, color, edgeCount){
+    final override fun drawShape() {  //ust classda open olan bir fonksiyonu alt classda final yaparsak final kalir. ama alt classda sifirdan yazilabilir
+        val square = "kare cizildi"
+    }
+}
 
+fun main() {
+    val shape = Shape("Shape")
+    val rectangle = Rectangle("rectangle")
+    val square = Square("Square", "Cyan",4)
 }
