fef8a63982d2b6657f7ce1b518cb48dd7697f61e
corepavlion <corepavlion@gmail.com>
2022-06-30 12:08:51 +0300

    first commit

--- src/main/kotlin/4 Classes/6 AbstractClasses.kt
+++ src/main/kotlin/4 Classes/6 AbstractClasses.kt
@@ -5,54 +5,65 @@ interface  McDonaldsService {
 }
 
 /**
- * abstract classlarin temel amaci sozlesmedir. abstract class sablon classlardir. kopya kagidi gibidir.
- * child classlarda yapilmasini zorunlu olarak koydugun yapilarin sablonudur..
- * bu sayede child classlarda fazla kod yazmaktan bizi kurtarir.
- *
- * abstract property(initial default degeri almaz) ve functionlar(body si olmaz) deger almazlar.
- * abstract class inherit alabilen classdir. Final yapmanin anlami olmaz
- *
- * eger abstracyt classiniza eklemek isteginiz opsiyonel yapilar varsa, bunlari child classlarin tamaminda override
-etmek zorunda olmayalim diye, open keyword u ile tanimlayabiliriz.
- * abstract classlarda normal open bir fonksiyon ve  degisken yazilmamali. cok gerekli degilse.
- * 1 classda kullanilacak open bir fonksiyonu yeni bir class extend ederek openi onun icine koymak mantiklidir.
- * final ve open modifierlari kullanmak anlamsiz ve yanlistir.
- *
- * abstract olarak yazilmis class a yeni eklenen tum abstract yapilar, tum child classlarda override edilmek zorundadir.
- *
- * bir abstract class in instance olusturamayiz. open bir class ile abstract class in farki budur.
-base class bu yuzden abstract yapilir ki instance olusturulamasin. singleton pattern abstract classlarla olusabilir.
- *
- * abstract class bir diger abstract class i inherit ediyorsa abstract yapilari override etmek zorunda degildir.
-ama child class abstract degilse mecburen ust abstract classdaki overridelari etmelisin
- *
- * eger bir abs class baska bir abs class tarafindan miras alindiysa bu kendi icinde override edilmesi zorunlu olan deg veya func override ederse yine override zorunlulugu kalkar.
-cunku override edilen bir function open olur body alir. hic override edilmesi istenmiyorsa final koyariz. override edilmez
- *
- *
+ 1-  abstract classlarin temel amaci sozlesmedir. abstract class sablon classlardir. kopya kagidi gibidir.
+ child classlarda yapilmasini zorunlu olarak koydugun yapilarin sablonudur..
+ bu sayede child classlarda fazla kod yazmaktan bizi kurtarir.
+
+
+ 2- abstract property(initial default degeri almaz) ve functionlar(body si olmaz) deger almazlar.
+
+ 3- abstract class inherit alabilen classdir. final ve open modifierlari kullanmak anlamsiz ve yanlistir.
+
+ 4- eger abstract classiniza eklemek isteginiz opsiyonel yapilar varsa, bunlari child classlarin tamaminda override
+ etmek zorunda olmayalim diye, open keyword u ile tanimlayabiliriz.
+ abstract classlarda normal open bir fonksiyon ve  degisken yazilmamali. cok gerekli degilse.
+ Bir abstract classda kullanilacak open bir fonksiyonu yeni bir class inherit ederek openi onun icine koymak mantiklidir.
+
+
+ 5- abstract olarak yazilmis class a yeni eklenen tum abstract yapilar,
+ tum child abs olmayan classlarda override edilmek zorundadir.
+
+ 6- bir abstract class in instance olusturamayiz. open bir class ile abstract class in farki budur.
+ base class bu yuzden abstract yapilir ki instance olusturulamasin.
+ bu sebeple singleton pattern abstract classlarla olusabilir.
+
+ 7- abstract class bir diger abstract class i inherit ediyorsa abstract yapilari override etmek zorunda degildir.
+ ama child class abstract degilse mecburen ust abstract classdaki overridelari etmelisin
+
+
+ 8- eger bir abstract class baska bir abstract class tarafindan miras alindiysa ve bu kendisi icerisinde,
+ zaten ust clasindaki herhangi bir override edilmesi zorunlu olan degiskeni veya functionu,
+ kendi icinde override ederse bu durumda bu class i yani child olaan abs classi,
+ miras alan duz bir classta, bu functionun yine override zorunlulugu ortadan kalkar.
+ cunku override edilen bir function open olur body alir.
+ childlarin birinden sonra hic override edilmesi istenmiyorsa final koyariz. override edilmez
+
  */
 abstract class  MCDonaldsFranchize {
-    //abstract val fridge: Fridge = Fridge() diyemeyiz cunku biz sablon veriyoruz. Belli bir buzdolabi istemiyoruz.
-                                    //Sadece buzdolabi olmasi gerekiyor. Franchize sozlesmesinde bir buzdolabin olsun der.
+    //abstract val fridge: Fridge = Fridge()  diyemeyiz cunku biz sablon veriyoruz. Belli bir buzdolabi istemiyoruz.
+    //Sadece buzdolabi olmasi gerekiyor. Franchize sozlesmesinde bir buzdolabin olsun der belli bir dolap belirtmez
 
     abstract val fridge: Fridge
     abstract val superVisor: SuperVisor
-//    abstract val employeeOne: Employee
-//    abstract val employeeTwo: Employee
-//    abstract val employeeThree: Employee
+    abstract val employeeOne: Employee
+    abstract val employeeTwo: Employee
+    abstract val employeeThree: Employee
     abstract val menuList:  List<McHamburger>
 
     abstract fun clean(clock: Int)
 
     //opsiyonel
-    open val policeOfficer: Police = Police()
+    open val policeOfficer: Police = TODO()
 
-    // bazi mcdonaldslar kahve hizmetide veriyor opsiyonel olarak kullanilabilir.
+
 }
 
-abstract class McDonaldsExpress : MCDonaldsFranchize(){  //bir abs diger abs class i miras alabilir
-    abstract  fun sellCoffee(): McCoffee
 
+
+abstract class McDonaldsExpress : MCDonaldsFranchize(){  //bir abs diger abs class i miras alabilir
+    abstract  fun sellCoffee(): McCoffee //bazi mcdonaldslar kahvede satiyor expresse kendi abst func ile kahvesatisinida ekliyoruz.
+                                        // open olarak class icinde yazmaktansa yeni bir child olusturup icinde tanimlamak onemli
+                                            //10 franxhizeda kahve yokken birinde varsa bunu base abstracta yazmak yerine boyle yapilmali
     override fun clean(clock: Int) {
         println("Clean time: $clock")
     }
@@ -61,17 +72,19 @@ abstract class McDonaldsExpress : MCDonaldsFranchize(){  //bir abs diger abs cla
         get() = TODO("Not yet implemented")
 }
 
+
+
 class McDonaldsMaltepe : MCDonaldsFranchize(), McDonaldsService { //bir duz class abs class i inherit alabilir ama ust classdaki butun abstract yapilari override etmek zorundadir.
     override val fridge: Fridge
         get() = TODO("Not yet implemented")
     override val superVisor: SuperVisor
         get() = TODO("Not yet implemented")
-//    override val employeeOne: Employee
-//        get() = TODO("Not yet implemented")
-//    override val employeeTwo: Employee
-//        get() = TODO("Not yet implemented")
-//    override val employeeThree: Employee
-//        get() = TODO("Not yet implemented")
+    override val employeeOne: Employee
+        get() = TODO("Not yet implemented")
+    override val employeeTwo: Employee
+        get() = TODO("Not yet implemented")
+    override val employeeThree: Employee
+        get() = TODO("Not yet implemented")
     override val menuList: List<McHamburger>
         get() = TODO("Not yet implemented")
 
@@ -85,16 +98,39 @@ class McDonaldsMaltepe : MCDonaldsFranchize(), McDonaldsService { //bir duz clas
 
 }
 
+class McDonaldsMaltepeExpress : McDonaldsExpress(){
+    override fun sellCoffee(): McCoffee {
+        TODO("Not yet implemented")
+    }
 
-fun main() {
+    override val superVisor: SuperVisor
+        get() = TODO("Not yet implemented")
+    override val employeeOne: Employee
+        get() = TODO("Not yet implemented")
+    override val employeeTwo: Employee
+        get() = TODO("Not yet implemented")
+    override val employeeThree: Employee
+        get() = TODO("Not yet implemented")
+    override val menuList: List<McHamburger>
+        get() = TODO("Not yet implemented")
+
+    override fun clean(clock: Int) {  //8. madde ornegi 2. child classda override old icin 3. child classta opsiyoneldir.
+        super.clean(clock)
+    }
 
 }
 
+
+fun main() {
+    //val mcdonalds = MCDonaldsFranchize()   olusturamazsin 6. maddeye bak
+}
+
+class Employee
 class Fridge
 class SuperVisor
 class Police
 class McHamburger
 class McCoffee
-//class Employee
+
 
 
--- src/main/kotlin/4 Classes/7 Interface.kt
+++ src/main/kotlin/4 Classes/7 Interface.kt
@@ -1,25 +1,29 @@
 package `4 Classes`
 
 /**
- * Interfacelerin, Abstract classlardan en buyuk farki state tutamiyor! oluslari.Yani icinde bir veri tanimlayip tutamiyoruz.
- * interface fonksiyonlari bir class icindeki hicbir yapiya ihtiyac duymazlar bir yerde tetiklenmeyi bekleyip baska bir yerde calismayi bekler.
- *
- * Interface icerisindeki initial degeri olmayan bir property(field) ya da body si olmayan bir fonksiyon
- * abstract olarak da tanimlanabilir.ama abstract tanimlama geregi yoktur.
- *
- * interfaceleri anlami geregi final yapamazsiniz. open yazilmasi da gereksizdir.(redundant)
- *
- * class lar inherit edilirken, interfaceler implement edilir. open koyma mantigi yoktur zaten amac implementtir.
- *
- * abstractlar; sozlesmeyken kesinlikle(override) sahip olmasi gerenkenleri soyler, default olarak yapilanlar
- * interfaceler; ise herhangi bir classin interface e sahip olmasi demek yeteneklerini artiryor demektir. harici beceri seti kazandirir
- *
- * Eger bir class a harici beceri seti kazandirmak isteniyorsa interface e yazmak gerekiyor.
- * abstract class a open function tanimlamak yerine interface ile yapilmalidir.
- * bu functionu interface verip class a implement edersin daha temiz kod yazmis olursun.
- *
- * open bir fonksyionu abstract icine yaziyorsak abstract icindeki bir bilgiye ihtiyac duyoyorsa tanimlariz. yani superi cagirarak is yapiyorsa tanimlanir.
- * fakat hicbirseye ihtiyac duymuyorsa abstract yerine interface tanimlamaliyiz.
+ 1- Interfacelerin, Abstract classlardan en buyuk farki state tutamiyor! oluslari.
+ Yani icinde bir veri tanimlayip tutamiyoruz.
+
+ 2- Interface icerisindeki initial degeri olmayan bir property(field) ya da body si olmayan bir fonksiyon
+ abstract olarak da tanimlanabilir.ama abstract tanimlama geregi yoktur.(redundant)
+
+ 3- interfaceleri anlami geregi final yapamazsiniz. open yazilmasi da gereksizdir.(redundant)
+
+ 4- abstractlar; sozlesmeyken kesinlikle(override) sahip olmasi gerenkenleri soyler, default olarak yapilmasi gerekli olanlar
+ interfaceler; ise herhangi bir classin interface e sahip olmasi demek yeteneklerini artiryor demektir. harici beceri seti kazandirir
+
+ 5- Eger bir class a harici beceri seti kazandirmak isteniyorsa interface e yazmak gerekiyor.
+ abstract class a open function tanimlamak yerine interface ile yapilmalidir.
+ bu functionu interface verip class a implement edersin daha temiz kod yazmis olursun.
+
+ 6- open bir fonksyionu abstract icine yaziyorsak abstract icindeki bir bilgiye ihtiyac duyoyorsa tanimlariz.
+ yani superi cagirarak is yapiyorsa tanimlanir.
+ fakat hicbirseye ihtiyac duymuyorsa abstract yerine interface tanimlamaliyiz.
+
+ 7- interface fonksiyonlari bir class icindeki hicbir yapiya ihtiyac duymazlar bir yerde tetiklenmeyi bekleyip baska bir yerde calismayi bekler.
+
+ 8- class lar inherit edilirken, interfaceler implement edilir. open koyma mantigi yoktur zaten amac implementtir.
+
  */
 
 interface MyInterface {
@@ -30,6 +34,7 @@ interface MyInterface {
     aslinda burada veri tutmuyoruz bu bir propertydir.
     property set get functionlari old icin interfacelerdede
     function tanimlandigi icin veri tutuyormusuz gibi oluyour.
+    Bu state tuttugu anlamina gelmez  Foo yu degistiremeyiz.
      */
     val propertyWithImplementation: String
     get() = "Foo"
