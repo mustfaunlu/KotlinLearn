12eddf99897ab54ec9eb7696af4e3819a41879c0
corepavlion <corepavlion@gmail.com>
2022-07-06 00:02:22 +0300

    first commit

--- src/main/kotlin/4 Classes/12 NestedInnerClass.kt
+++ src/main/kotlin/4 Classes/12 NestedInnerClass.kt
@@ -4,10 +4,11 @@ package `4 Classes`
  * Nested Classlar outer(dis) class in uye propertylerine ya da fonksiyonlarina erisemezler.
  * Nested classlar static olarak saklanirlar.  Bundan dolayi da outer class uzerinden nested class a
  erismek icin Outer.NestedClass() seklinde erisebiliriz.
+ * Nested class in icerisine ise bir nested ya da inner fark etmeksizin class ekeleyebiliriz.
  *
  * Inner class a baktigimizda ise static olarak tutulmazlar.. Bundan dolayi erismek icin
  Outer().InnerClass() seklinde outer class in intance indan erisiriz.
- * Inner class in icine ye ni bir nested class eklenemez.
+ * Inner class in icine yeni bir nested class eklenemez. Ancak yine inner class eklenebilir.
  */
 
 class Outer {
@@ -16,11 +17,17 @@ class Outer {
 
     fun getAge(): Int = 25
 
-    class NestedClass {   // genelde tek dosyadan farkli classlari gorebilmek icin yazilirlar.
+    class NestedClass {   // genelde tek dosyadan farkli classlari gorebilmek icin yazilirlar
         fun printName(){
-            //name     outer class in hicbir uyesine erisemeyiz
+            //name     outer class in hicbir uyesine erisemeyiz cunku NestedClass statictir
             //surName
             //getAge()
+        }
+
+        fun printName2(outer: Outer){   //direk memory leak bunu yapmak gunahtir. :))) bunu yapmak yerine inner yap!!
+            outer.name
+            outer.surName
+            outer.getAge()
         }
     }
 }
@@ -36,8 +43,9 @@ class  Outer2 {
 
     fun getAge() : Int = 24
 
-    inner class InnerClass {
-        fun printName() { //static olsaydi Outer2 de memoryde yer kaplayacak ve memory leak e neden olacakti
+    inner class InnerClass { //static olsaydi Outer2 de memoryde yer kaplayacak garbage collector silmeyecekti
+        // ve memory leak e neden olacakti
+        fun printName() {
             name
             surName
             getAge()
@@ -50,11 +58,16 @@ fun main() {
     outer.surName
 
     Outer.NestedClass().printName()
-    //Outer().NestedClass().printName()  boyle yazarsak nested outer in bir parcasi gibi olur o yuzde instance ile erisemeyiz.
+
+    //asagidaki gibi yazarsak nested outer in bir parcasi gibi olur o yuzde instance ile erisemeyiz.
+    //outer.NestedClass().printName()
+    //Outer().NestedClass().printName()
+
 
     val outer2 = Outer2()
     outer2.surName
     outer2.getAge()
+    outer2.InnerClass().printName()
     Outer2().InnerClass().printName()
 
 }
--- src/main/kotlin/4 Classes/15 object Singleton.kt
+++ src/main/kotlin/4 Classes/15 object Singleton.kt
@@ -1,5 +1,26 @@
 package `4 Classes`
 
+
+
+/**  IS GORUSMESI KESIN SORUDUR!!
+                          *** singleton pattern nedir??? ***
+* basitce herhangi bir t aninda bir class in nesnesine erismek istedigimizde
+bize her zaman ayni nesne donecegini garanti eden design patterndir.
+
+* bir classin memoryde her erisildiginde ayni nesneyi vermesi yani memoryde bir obje tutar o class hep ayni nesneyi verir.
+
+                         *** bir objeye kac kere erisildigini ogrenmek icin ne yapabilirim??? ***
+* singleton pattern yapariz. pattern icine counter degiskeni koyup her eristigimde bir artiririm.
+
+
+                            * singleton pattern dezavantaji nedir?
+ * amacina uygun kullanilmazsa mesela veri tasima icin kullanilirsa memoryde silinemeyen birsuru alan isgal edilmis olacak.
+sonuc olarak memory leak(outofmemory) orani artacak. Antipattern olmaya musaittir.
+ *
+ *
+ */
+
+
 object ProfileKotlin{
     var nickName: String? = null
 }
@@ -7,3 +28,15 @@ object ProfileKotlin{
 fun main() {
     ProfileKotlin.nickName = "Musti"
 }
+
+
+
+
+
+
+/**
+ * once is yap
+ * teknik detaylar
+ * platform detaylari
+ * patternler architectial mimari - design pattern
+ */
