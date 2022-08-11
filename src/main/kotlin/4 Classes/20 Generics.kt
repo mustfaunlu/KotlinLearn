798fe749bbcf4d9451cbf16005a0fc0405ab5e6b
corepavlion <corepavlion@gmail.com>
2022-08-11 22:52:32 +0300

    first commit

--- src/main/kotlin/4 Classes/20 Generics.kt
+++ src/main/kotlin/4 Classes/20 Generics.kt
@@ -1,2 +1,139 @@
 package `4 Classes`
 
+/**
+ * Auto -> MiniCooper -> John -> JohnCabrio
+ *      -> Beetle
+ *
+ * Bycle -> Bisan -> BlackBisan
+ *       -> Corelli
+ */
+
+/**
+ * hiyerarsiyi kisitlamak icin kullanilan mukemmel bir ozellik
+ * olaya buyuk cerceveden bakmaya saglar.
+ * parametre olan yerlerde kullanialbilir.
+ * constructorlarda fonksiyonlarda higher orderlarda extensinon inline functionlarda
+ *
+ *
+ *
+ * out verilirse class in kendisi ve childlari , in verilirse class in kendisi ve ust classlari
+ *
+ *
+ * reified keyword u sadece inline functionlarda kullanilir.
+ * generic tipler derleme zamaninda hangi tipte oldgunun bilgisinde degildir.
+ * run tusuna basildigin hala ide hangi tipte old bilmez.
+ * ne zaman kod run time da orasi okunur oyle haberi olur.
+ * reified kullanirsak derleme zamaninda tipi bilinir.
+ * bunun hangi tip olacagi bilinirse  generic tiplerle ilgili 2 onemli kontrol var
+ * 1 generic tipin hangi tipte oldugunu anlamak icerde iki tipide kullaniyor olabilir.
+ * bunun icin tipi belli olmali reified bunun belli olmasini saglar.
+ */
+
+interface TransportationFunctions {
+    fun run()
+}
+
+open class Auto : TransportationFunctions {
+    open val namec: String = ""
+    open val color: String = ""
+    override fun run() {
+        println("run runn")
+    }
+
+}
+
+open class Bycle {
+    open val namec: String = ""
+    open val color: String = ""
+
+}
+
+//===================================================
+open class MiniCooper(
+    override val namec: String = "MiniCooper", override val color: String = "Red"
+) : Auto() {
+
+    override fun toString(): String {
+        return "name $namec , color: $color"
+    }
+
+    init {
+        super.run()
+    }
+}
+
+open class John : MiniCooper() {
+    init {
+        super.run()
+    }
+}
+
+class JohnCabrio : John() {
+    init {
+        super.run()
+    }
+}
+
+class Beetle(
+    override val namec: String = "Beetle",
+    override val color: String = "Yellow"
+) : Auto() {
+    override fun toString(): String {
+        return "name $namec color: $color"
+    }
+
+    init {
+        super.run()
+    }
+}
+
+open class Bisan(
+    override val color: String = "Bisan",
+    override val namec: String = "Brown"
+) : Bycle(), TransportationFunctions {
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+
+    override fun toString(): String {
+        return "name $namec color $color"
+    }
+}
+
+class BlackBisa() : Bisan()
+
+class Corelli(
+    override val namec: String = "Corelli",
+    override val color: String = "Gray"
+) : Bycle() {
+    override fun toString(): String {
+        return "name: $namec color: $color"
+    }
+}
+
+
+class TransportFactory<out T: Auto>(transportationVehicle: T) {
+    init {
+        println(transportationVehicle.toString())
+    }
+}
+
+fun main() {
+    val miniCooper: MiniCooper = MiniCooper()
+    val john: John = John()
+    val johnCabrio = JohnCabrio()
+    val beetle: Beetle = Beetle()
+
+    val bycle = Bycle()
+    val bisan = Bisan()
+    val blackBisan = BlackBisa()
+    val corelli = Corelli()
+
+    val miniCooperFActory: TransportFactory<MiniCooper> = TransportFactory<MiniCooper>(miniCooper)
+    val johnFActory: TransportFactory<John> = TransportFactory<John>(john)
+    val johnCabrioFActory: TransportFactory<JohnCabrio> = TransportFactory<JohnCabrio>(johnCabrio)
+    val beetleFActory: TransportFactory<Beetle> = TransportFactory<Beetle>(beetle)
+
+    val johnFactory3: TransportFactory<John> = TransportFactory<JohnCabrio>(johnCabrio)
+    val johnFactory4: TransportFactory<John> = TransportFactory<John>(john)
+}
--- src/main/kotlin/4 Classes/21 Reflection.kt
+++ src/main/kotlin/4 Classes/21 Reflection.kt
@@ -1,2 +1,75 @@
 package `4 Classes`
 
+import java.lang.reflect.Field
+
+/**
+ * private field lara erismek icin kullanilir.
+ * kendi class inda reflection yapmak sacma olur thirth party kutuphanelerde kullanilabilir.
+ * Kullanirken progguard dan kaynakli sorunlara dikkat etmek lazim.
+ * Cok lazimsa kullanilmali.
+ *
+ *     * !NOTTT
+ * Eger yazilan uygulama kodlarken sikintisiz calisiyorsa apkyi imzalarken proguarddan gecirdigimiz durumda
+ * problemli calisiyorsa kesinlikle ilk bakmamiz gereken yer icerde calistigimiz sdk veya biz reflection kullaniyoruzdur.
+ * isimler proguardda degistigi icin sikinti vardir.
+ *
+ */
+
+class Walpaper {
+    private var imageUrl: String = "www.unludev.com"
+    private var id: Int = 1232
+    private var type: Int = 2
+    private var width: Int = 1399
+    private var height: Int = 970
+    private var isGif: Boolean = false
+    private var previewUrl: String = "www.unludevprw.com"
+
+    private fun printer() {}
+}
+
+fun main() {
+    val walPaper = Walpaper()
+    /**class in instance olusturup*/
+//    walPaper.imageUrl
+//    walPaper.id
+//    walPaper.type
+//    walPaper.width
+//    walPaper.height
+//    walPaper.isGif
+//    walPaper.previewUrl
+    //println(walPaper.imageUrl)
+    /**instance uzerinden javaClass a gectik
+     * getDeclaredField fonksiyonuna ulasmak istedigmiz degiskenin ismini string olarak veriyoruz.
+     * reflection kullanabilmek icin degisken isimlerini bilmemiz gerekiyor ve bu degisken isimleri degismemeli
+     * butun bunlar okey olduktan sonra ilgili fieldi isAccessible function ile true yapiyoruz.
+     * boylece artik degisken erisebiliriz. fieldIsmi.get(nesneIsmi) seklinde private field a erismis olruz.
+     *
+     * bu kullanimda apk dosyasini proguard ile obfuscate ederken sikinti cikabilir.
+     * cunku obfuscate yapilirken proguard propery isimlerini kafasina gore harflere mapler.
+     * bu ornekte imageUrl propertysinin ismini rastgele atadigi harfe maplerse getDeclaredField("imageUrl") olarak kalir.
+     * cunku proguard String ifade ile yazilmis olan ifadenin propery olacagini anlayamaz.
+     * bunun onune gecmek icin proguard in rulesetlerine tanimlamayi unutmamiz gerekir.
+     *
+     *
+
+     * */
+    val imageUrlField: Field = walPaper.javaClass.getDeclaredField("imageUrl")
+    imageUrlField.isAccessible = true
+    val imageUrlString = imageUrlField.get(walPaper)
+    println(imageUrlString)
+
+
+//2. yontem
+    /**
+     * bu yontemde class icindeki propertylerin siralamasinin degismeyecegini garanti etmemiz gerekir
+     * comment ile bilgi verilmesi gerrekir yeri degismemesi icin.
+     * cunku class siralamasi uzerinden index e gore property e ulasiyoruz.
+     * */
+    val imageUrlFieldName = walPaper.javaClass.declaredFields[0].name
+    val imageUrlField2 = walPaper.javaClass.getDeclaredField(imageUrlFieldName)
+    imageUrlField2.isAccessible = true
+    val imageUrlString2 = imageUrlField.get(walPaper)
+    println(imageUrlString2)
+
+
+}
