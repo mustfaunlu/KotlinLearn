678b06f0dd978cb9eda4b787b16bcc004fc3d94a
corepavlion <corepavlion@gmail.com>
2022-08-29 19:53:36 +0300

    last commit

---
+++ src/main/kotlin/5 - Scoope Functions/apply.kt
@@ -0,0 +1,20 @@
+package `5 - Scoope Functions`
+
+/**
+ * contextObject.apply {(this)..lambda..} return contextObject
+ * apply; nesnesi yeni olusturulan nesnenin propertylerinin ilk deger atamalarinin yapilmasinda kullanilir. Nesneyi ozellestirmek
+icin kullanilir. constructorundan parametre alan class ile kullanilmasi yanlis. duz class ile kullanilabilir.
+constructorda olmayan parametreleri doldurmak icin kullanilir. Olmayan propertyleri olusturmak ve initiliaze etmetk icin
+gerekli fonksiyonlari tanimlayabiliriz. Apply kisaca aldigi contextObjecti kullanima hazir hale getirmeye yarar.
+ * Bir nesneyi yapilandirmayi saglar.
+ *'nesneye asagidaki atamalari uygula' gibi bir anlami vardir.
+
+        val adam = Person("Adam").apply {
+            age = 32
+            city = "London"
+        }
+        println(adam)
+
+ * Alıcıyı dönüş değeri olarak alarak, daha karmaşık işlemler için uygulamayı
+ kolayca çağrı zincirlerine dahil edebilirsiniz.
+ */
