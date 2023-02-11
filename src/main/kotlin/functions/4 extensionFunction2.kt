534d10f3c35fd91679694d47afd1b545a1c3d924
unlu <corepavlion@gmail.com>
2023-02-11 10:43:23 +0300

    repeat and modify knowledge

--- src/main/kotlin/functions/4 extensionFunction2.kt
+++ src/main/kotlin/functions/4 extensionFunction2.kt
@@ -1,5 +1,16 @@
 package functions
 
+
+
+/**
+ * X sinifi icinde, Y sinifa ait extension fonksiyon yazilirsa, X sinifi icinde ayni isimde uye
+ *  fonksiyonu olsa bile, extension fonksiyonun icerisinde, ayni isme sahip uye fonksiyon cagrilirsa, bu extension
+ * fonksiyona isaret eder. X Sinifinin ayni isimdeki uye fonksiyonunu extension fonksiyon icersinde cagirmak icin
+ * this@X .uyefonksiyon() seklinde cagirim yapilmalidir.
+ *
+ * Buradaki ornekte; Shape sinifinda hem duz bir  extToString() fonksiyonu var,
+ * hemde Int.extToString() seklinde extension fonksiyon var.
+ * **/
 open class Shape {
 
     var intNumber = 0
@@ -8,29 +19,21 @@ open class Shape {
         this.intNumber = intNumber
     }
 
-    fun main() {
+    fun a() {
         intNumber.extToString()
         intNumber.log("")
     }
 
-/* -------------------------------------------------------------------------------------------------------------------*/
-
-    /**
-     *      X sinifi icinde, Y sinifa ait extension fonksiyon yazilirsa, X sinifi icinde ayni isimde uye
-     *       fonksiyonu olsa bile, extension fonksiyonun icerisinde, ayni isme sahip uye fonksiyon cagrilirsa, bu extension
-     *      fonksiyona isaret eder. X Sinifinin ayni isimdeki uye fonksiyonunu extension fonksiyon icersinde cagirmak icin
-     *      this@X .uyefonksiyon() seklinde cagirim yapilmalidir.
-     * **/
     open fun Int.extToString() {
         println("")
 
         // Int.extToString() methodunu isaret eder.
         extToString()
         this.extToString() //aslinda shape class icinde gibidir ama buradaki this Int classini isaret eder.
-                            // Cunku extension fonksiyon icindeyken receiver class icindeyizdir.
-                            // Normal functionda this icinde old. classi isaret ederdi.
+                            // Cunku extension fonksiyon scope'u icindeyken receiver class icindeyizdir.
+                            // Normal functionda this icerisinde bulundugu classi isaret ederdi.
         //ama biz bu extToString i sadece Shape class i icinde kullanabiliriz. Butun projede kullanmayiz.
-        //butun projede ext fun kullanmak istiyorsak globalde yazmamiz gerekiyor.
+        //butun projede ext fun kullanmak istiyorsak kt dosyasinda class disinda yazmamiz gerekiyor.
 
 
 
@@ -51,7 +54,8 @@ open class Shape {
 /**
  *Bir sinif, kendinden turetilmis disarda yazilmis extension fonksiyonun
  aynisini (isim ayni, parametre sayisi ve tipleri ayni, ayrica geri donus tipi de ayni olmali),
- kendi icinde barindiriyorsa, bu durumda yazilan extension fonksiyon gecersizdir asla calismaz. Daima Sinifin uye fonksiyonu cagirilir.
+ kendi icinde barindiriyorsa, bu durumda yazilan extension fonksiyon gecersizdir asla calismaz.
+Daima Sinifin uye fonksiyonu cagirilir.
   **/
 fun Shape.setNumber(intNumber: Int) { //pratikte ext ederek shape class i icinde old icin shape in private degiskenine bile erisebliriz.
     val result = intNumber * intNumber //fakat bu ext func gecersizdir. yok hukmendidir. classin kendi funksiyonu calisir.
@@ -62,10 +66,12 @@ fun Shape.setNumber(intNumber: Int) { //pratikte ext ederek shape class i icinde
 
 /**
  * Bir sinifa extension fonksiyon yazilabildigi gibi extension property de yazilabilir.
- * Bunun sebebi aslinda property'lerin get() ve set() methodlarindan olusmasindan dolayidir.
- * Bu extension property'lerin icerisinde field tanimlanamaz.
+ * Bunun sebebi aslinda property'lerin ozunde bir fonksiyon olmasidir.
+ * Bu extension property'lerin icerisinde field tanimlanamaz. Yani deger atanamaz.
  * Dolayisiyla aslinda gercek anlamda bir degisken extension yapilamaz.
  *      Bu konu property vs field konusu ile beraber sinif'lar islenirken detayli anlatilacaktir. (onemli!!)
+ *      Aslinda biz kotlinde degiskenlerin fieldina erisemiyoruz, fielda erisen get ve set methodlarina
+ *      erisiyoruz. Bu fieldlara backing field denir.
  * **/
 var Shape.type
     get() = "Rectangle"
