5f3e62ab9ad662bab73b059a3e90073bcab888d2
unlu <corepavlion@gmail.com>
2023-03-01 20:37:58 +0300

    repeat and modify knowledge

--- src/main/kotlin/classes/16object expression.kt
+++ src/main/kotlin/classes/16object expression.kt
@@ -9,26 +9,41 @@ open class User(val age: Int){
 }
 
 /**
- * arka planda object kullanim bir class icinde olur.
  *
- *
+ * herhangi bir yapiya ait degiskenlerin icine bir class i atayabilmemize expression kullanim denir.
  * bir tip belirtmeden direk object i bir degiskene atarsak arka planda isimsiz  Any den miras alan bir class olusturmus oluruz.
  * Buna teknik makalelerde kullan at class denir. Bu javadaki structure kavramina denk gelir.
- *
- * Ayrica harici dosya icerisinde direk bir degiskenede atayabiliriz class icindede kullanabiliriz.
- * Ayrica dosya icinde tek kullanirsak arka planda static olur. Hic bir yapinin membere olmadigi icin.
- *
  * objenin expression kullaniminda bir interface implement edersek veya class extend edersek,
  * degiskene tip atamaya gerek yok zaten tipini otomatik alir. Fakat hem implement edip hem extend edersek yani iki tane
  * super type atarsak, degiskene tipinin ne oldugunu vermemiz gerekir.
  *
  *
- * bir interface(Print2) asagdaki gibi parametre olarak verilirse arka planda normalde yapamadigimiz sekilde nesnesi olusturulur.
+ * Ayrica harici dosya icerisinde direk bir degiskenede atayabiliriz class icindede kullanabiliriz.
+ Asagidaki fullname ornegindeki gibi dosya icinde tek kullanirsak arka planda static olur.
+ Hic bir yapinin membere olmadigi icin. Uygulamanin hayati boyunca memoryde tutulur bu sikinti yaratabilir.
+ Gerekli durumlarda dosya icinde tek basina kullanilmalidir haricinde class icinde veya baska bir supertype icinde kullanilmalidr.
+ *
+
+ *
+ *
+ * bir interface(Print2) asagdaki gibi implement edilirse arka planda normalde yapamadigimiz sekilde nesnesi olusturulur.
  * higher order fonksiyonlarida parametre olarak verdigimizde arka planda cagrildikca nesnesi olusturulmamasi icin inline crossline noline keywordlerini kullaniriz.
  *
  *Aslinda basitce bir degiskene bir class i esitliyormusuz ve butun class ozellliklerini kullanabiliyormusuz gibi aciklanabilir.
  *
  */
+
+
+    val helloWorld = object {
+        val hello = "Hello"
+        val world = "World"
+        // object expressionlar arka planda Any'yi extend eder,
+        // bu nedenle Any'den gelen `toString()` methodunu override edebilri.
+        override fun toString() = "$hello $world"
+    }
+
+
+
 val fullName: User = object : Print2, User(23){
 
     val firstName = "Mustafa"
@@ -48,6 +63,9 @@ val fullName: User = object : Print2, User(23){
 }
 
 fun main() {
+    print(helloWorld)
+
+
     // fullName.onPrint()  degisken tipi Print2 yapsaydik erisirdik.
     fullName.getUserFullName()  // degisken tipi User old. icin Userin memberlarina erisebiliyourz.
     //fullName.firstName  object in memberlarina asla disaridan erisemeyiz. Cunku fullName User class inin instance old icin sadece User  memberlarina erisir.
