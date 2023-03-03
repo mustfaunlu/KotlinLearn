35c1efaf2a65fa0ea777f4234b3e87db7778c91a
unlu <corepavlion@gmail.com>
2023-03-04 00:05:02 +0300

    repeat and modify knowledge

--- src/main/kotlin/classes/18Delegated- Property.kt
+++ src/main/kotlin/classes/18Delegated- Property.kt
@@ -1,6 +1,65 @@
 package classes
 
-/**
+/*
+ *                                 DELEGATION
+
+ Delegasyon paterninin implementasyon inheritance'ına iyi bir alternatif olduğu
+kanıtlanmıştır ve Kotlin bunu sıfır boilerplate kodu gerektiren yerel olarak desteklemektedir.
+ Bir Derived sınıfı, tüm public üyelerini belirli bir nesneye delege ederek bir
+Base interface'ini implemente edebilir:
+ */
+interface Base {
+    fun print()
+}
+
+class BaseImpl(val x: Int) : Base {
+    override fun print() {
+        print(x)
+    }
+}
+
+class Derived(b: Base) : Base by b
+
+fun main2() {
+    val b = BaseImpl(10)
+    Derived(b).print() //10
+}
+
+/*
+    Derived için süper tip listesindeki by-clause, b'nin Derived nesnelerinde dahili olarak
+saklanacağını ve derleyicinin Base'in b'ye iletilen tüm metodlarını oluşturacağını gösterir.
+
+
+            --- Delegasyon tarafından implement edilen bir interface üyesini override etme ---
+    Override'lar beklediğiniz gibi çalışır: derleyici, delege nesnesindekiler yerine sizin override
+ implementasyonlarınızı kullanır. Derived2'a override fun printMessage() { print("abc") } eklemek isterseniz,
+ printMessage çağrıldığında program 10 yerine abc yazdıracaktır:
+ */
+
+interface Base2 {
+    fun printMessage()
+    fun printMessageLine()
+}
+
+class BaseImpl2(val x: Int) : Base2 {
+    override fun printMessage() { print(x) }
+    override fun printMessageLine() { println(x) }
+}
+
+class Derived2(b: Base2) : Base2 by b {
+    override fun printMessage() { print("abc") }
+}
+
+fun main3() {
+    val b = BaseImpl2(10)
+    Derived2(b).printMessage() //abc
+    Derived2(b).printMessageLine() //10
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+
+/**                                        DELEGATION PROPERTIES
  * Uygulamanin her hangi bir aninda direkt olarak ihtiyac olmayan tum degiskenleri
  * daha sonra kullanildigi andan itibaren memoryde yer kaplama  kullanimina lazy denir.
  *
@@ -18,7 +77,7 @@ oraya basilmadikca calismasini istemedigimiz degiskenleri lateinit ya da by lazy
  * bylazy threadsafe mode ile ayni anda iki thread ayni anda obje olusturmasin diye kullanilabilir.z
  *
  * lateinitte bir lazy gibi init blogu yok. lateinitte ne zaman erisim saglanacagini belirlemek icin this::user2.isInitialize seklinde kontrol
- ederek yapariz.
+ederek yapariz.
  * ifInitialized kontrolu yapilmazsa ikitane user2  objesi olur mantik hatasi olur. ilk nesneye erisim kaybolur yeni bir nesne olusur.
  *
  */
@@ -51,6 +110,7 @@ class CardDetailFragment {
         }
         println("name: ${user2.name}, surname: ${user2.surName}")
     }
+
     init {
         userType = "Premium" //atama burada yapildiktan sonra lazy ve late init degiskenleri null dan Premiuma gecer.
 
@@ -60,7 +120,6 @@ class CardDetailFragment {
         // mesela class ayaga kalkti ui cizildi uida birde pop up var oraya basilmadikca calismasini istemedigimiz degiskenleri lateinit ya da by lazy kullaniriz.
     }
 }
-
 
 
 fun main() {
