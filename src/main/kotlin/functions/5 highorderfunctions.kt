368f2d1d4a3c3f2ee4cda2b516384e52d6fdca70
Mustafa ÜNLÜ <mustafaunlu@mail.com>
2023-04-08 23:28:58 +0300

    refresh one's knowledge

--- src/main/kotlin/functions/5 highorderfunctions.kt
+++ src/main/kotlin/functions/5 highorderfunctions.kt
@@ -8,13 +8,14 @@ fun main() {
 
     /**
      *      Fonksiyon'lar Kotlin'de "First Class"dır. Yani degiskenlere deger olarak atanabilir, baska fonksiyonlara
-     *      parametre olarak verilebilir ya da bir fonksiyonunn geri donus degeri olabilir demektir.
+     *      parametre olarak verilebilir ya da bir fonksiyonun geri donus degeri olabilir demektir.
      *
      *      Higher Order Function'lar basitce bir fonksiyona parametre olarak verilen fonksiyonlardır. Parametre olarak
      *      verilmekten kasit, fonksiyonun cagriminin parametre kisminda yapilmasi degil, fonksiyonun govdesinin (body)
      *      yani suslu parantezler arasinda kalan gorev alaninin baska bir fonksiyona parametre olarak verilmesidir.
      *
      *      Yapisal olarak;
+     *
      *
      *      fun foo(higherOrderFunction: (message: String) -> Unit){
      *          some business logics
